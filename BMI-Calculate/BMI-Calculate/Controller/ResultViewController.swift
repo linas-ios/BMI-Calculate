52eedb6aa987f7560c1d55efa38e5230539e977d
linas-ios <111570883+linas-ios@users.noreply.github.com>
2022-11-26 19:45:19 +0100

    create 'ResultViewController', add IBOutlets and IBAction

--- BMI-Calculate/BMI-Calculate/Controller/ResultViewController.swift
+++ BMI-Calculate/BMI-Calculate/Controller/ResultViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
-    override func viewDidLoad() {
+  @IBOutlet weak var bmiLabel: UILabel!
+  
+  @IBOutlet weak var showTipsLabel: UILabel!
+  override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  @IBAction func recalculateTapped(_ sender: UIButton) {
+  }
+  
 
 }
